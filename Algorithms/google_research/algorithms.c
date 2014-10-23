6faf5782004dc9661aaad5c7a43cafccd836e179
Sabesan Saidapet Pachai <sabesansp@gmail.com>
2014-10-22 23:53:48 -0700

    algorithm for writing a billion numbers to file

---
+++ Algorithms/google_research/algorithms.c
@@ -0,0 +1,29 @@
+// All libraries to be included here 
+# include<stdio.h>
+# include<stdlib.h>
+
+
+
+// P1 : Write a billion (10^9) numbers into a file
+void write_billion(char *file_name)
+{
+   FILE* fp;
+   
+   fp = fopen(file_name,"w");
+   // execute the for loop a billion times
+   for(unsigned int i=1;i<=1000000000;i++) 
+   {
+      fprintf(fp,"%d\n",i);
+   }
+   fclose(fp);
+}
+
+
+	
+// entry point of code execution
+int main()
+{
+   write_billion("/Volumes/My Passport/data/billion.txt");
+   return 0;
+}
+
