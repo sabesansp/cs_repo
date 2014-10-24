2010c2b14e94de8b9e10d6b5a716648db99b9309
Sabesan Saidapet Pachai <sabesansp@gmail.com>
2014-10-24 10:12:47 -0700

    segmentation fault

--- Algorithms/google_research/algorithms.c
+++ Algorithms/google_research/algorithms.c
@@ -5,6 +5,7 @@
 
 
 // P1 : Write a billion (10^9) numbers into a file
+// This operation took approximately 5 mins
 void write_billion(char *file_name)
 {
    FILE* fp;
@@ -19,33 +20,51 @@ void write_billion(char *file_name)
 }
 
 
-// SP : returns the maximum element in "cost" which is less than "sum"
-// make sure that "cost" is in a sorted order
-int compute_max_element_less_than_element(int sum, int* cost, int n)
+// SP : returns the maximum element in "a" which is less than "s"
+// make sure that "a" is in a sorted order
+int compute_max_element_less_than_element(int s, int* a, int l,int r,int n)
 {
-   // perform a binary search here
-   // design issue : how do you know where to stop in the set of elements
-   // that cost points to 
-   int i = 0, left = 0, right = n-1;
+   // compute mid-point 'm'
+   int m = (l+r)/2;
    
+   if(a[m]<=s)
+   {
+      if(m==(n-1) || a[m+1]>s) 
+         return a[m];
+      if(m<(n-1))
+      {
+         if(a[m+1]<=s)
+         {
+            return compute_max_element_less_than_element(s,a,m+1,r,n);
+         }
+      }
+   } else 
+   {
+      if(m>0)
+      {
+         return compute_max_element_less_than_element(s,a,l,m+1,n);
+      }
+   }   
 }      
 
 
 
 // P2 : dynamic programming problem
-int compute_min_number_of_coins(int sum,int* cost, int n)
+int compute_min(int s,int* a, int n)
 {
-   if(sum == 0) 
+   if(s == 0) 
       return 0;
    else 
-      return 1 + compute_min(
+      return 1 + compute_min(s - compute_max_element_less_than_element(s,a,0,n-1,n),a,n);
 
 }
 	
 // entry point of code execution
 int main()
 {
-   write_billion("/Volumes/My Passport/data/billion.txt");
+   //write_billion("/Volumes/My Passport/data/billion.txt");
+   int a[] = {5,9,10,11,13,15};
+   printf("%d",compute_max_element_less_than_element(12,a,0,5,6));
    return 0;
 }
 
