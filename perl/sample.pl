04783454d2d9016a4fa3215bf484a1a4126f9d15
Sabesan Saidapet Pachai <ssaidapetpach@htb-1n-eng-dhcp68.eng.vmware.com>
2015-02-02 18:10:42 -0800

    update_02_02_2015

---
+++ perl/sample.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+my $array =  [];
+
+
+my $result = $array;
+
+
+#print(@$array."\n");
+
+if(@$result == 0) {
+
+   print("Empty array\n");
+}
+else {
+
+   print("Not equal\n");
+
+}
