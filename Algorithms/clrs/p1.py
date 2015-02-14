8a3d867a2f418f94b434f4ed5ebff8b8a2872218
Sabesan Saidapet Pachai <sabesansp@Sabesan-Saidapet-Pachais-iMac.local>
2015-02-14 02:38:08 -0800

    Initial commit of files : p1.py, data.txt,python-doc

--- Algorithms/clrs/p1.py
+++ Algorithms/clrs/p1.py
@@ -1,1 +1,38 @@
-def   
+## How much time does it take to load a list of 10000 integers ? 
+## [using an intel i3 processor(3.2Ghz) with 4GB RAM]
+
+
+from random import randint
+
+
+## Method to create a dataset of "num_int" integers
+## and store them in a file
+
+def create_dataset(num_int,file_path):
+
+   # open a file in write mode
+   
+   f = open(file_path,'w')
+
+   # write "num_int" random integers into file
+
+   for i in range(1,num_int+1):
+
+      s = randint(1,num_int)
+      f.write(str(s))
+      f.write("\n")
+
+   f.close()
+
+
+
+
+
+
+if __name__ == '__main__' :
+
+   # "data.txt" file exists in the current working directory
+   # created through create_dataset api
+
+  
+  
