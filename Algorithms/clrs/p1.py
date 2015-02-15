5c7c355f62f9d9d681b27d2e0675b7cf098f26be
Sabesan Saidapet Pachai <sabesansp@Sabesan-Saidapet-Pachais-iMac.local>
2015-02-15 00:05:23 -0800

    update_insertion_sort_algorithm

--- Algorithms/clrs/p1.py
+++ Algorithms/clrs/p1.py
@@ -3,6 +3,7 @@
 
 
 from random import randint
+from time import time
 
 
 ## Method to create a dataset of "num_int" integers
@@ -26,13 +27,81 @@ def create_dataset(num_int,file_path):
 
 
 
+## Display the list of integers
+
+def display(int_list) :
+
+   for i in range(0,len(int_list)) :
+
+      print int_list[i]
 
 
+
+## Load the dataset from the "file_path"
+
+def load_dataset(file_path) :
+
+   # open the file in read mode
+
+   f = open(file_path,'r')
+
+   # read lines from file
+ 
+   lines = f.read().splitlines()
+
+   int_list = []
+
+   # Iterate through lines 
+
+   for line in lines :
+
+      int_list.append(int(line))
+
+
+   f.close()
+
+   return int_list
+
+
+## Method to measure the time elapsed
+## for a certain function 'f'
+
+def measure_time(func,data) :
+
+   start_time = time()
+   func(data)
+   stop_time = time()  
+   elapsed_time = stop_time - start_time
+   return elapsed_time
+
+
+
+## Method to perform insertion sort 
+## on an array of integers
+
+def insertion_sort(int_list) :
+
+   for j in range(1,len(int_list)):
+      key = int_list[j]
+      i = j-1
+      while (i>=0 and key<int_list[i]) :
+         int_list[i+1] = int_list[i]
+         i = i-1  
+      int_list[i+1] = key
+
+
+## Actual entry point of the program
 
 if __name__ == '__main__' :
 
    # "data.txt" file exists in the current working directory
    # created through create_dataset api
 
+   int_list = load_dataset('data.txt')
+
+   t_insertion_sort = measure_time(insertion_sort,int_list)
   
-  
+   display(int_list)
+
+   print "time_insertion_sort = ",t_insertion_sort
+
