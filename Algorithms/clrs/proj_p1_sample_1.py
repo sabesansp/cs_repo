0d367d3ded0b11e89578c3823309769a07d87d3c
Sabesan Saidapet Pachai <ssaidapetpach@htb-1n-eng-dhcp77.eng.vmware.com>
2015-02-19 17:51:16 -0800

    update_1_02_19_2015

--- Algorithms/clrs/proj_p1_sample_1.py
+++ Algorithms/clrs/proj_p1_sample_1.py
@@ -5,6 +5,21 @@ def sum(a,b) :
    c = a + b
    return c
 
+def find_function(func_name,file_name) :
+
+
+   try :
+   
+      fp = open(file_name)
+
+   except IOError : 
+
+      return None
+
+   line = fp.readline()
+   print "line = ",line
+   fp.close()
 
 if __name__ == '__main__':
    print sum(5,10)
+   find_function('find_function','p1.py')
