c60f1a89a45f8c1d5ddbd81a339a4e6c3c01b056
Sabesan Saidapet Pachai <ssaidapetpach@htb-1n-eng-dhcp389.eng.vmware.com>
2015-02-25 17:57:06 -0800

    update_1_feb_25_2015

--- Algorithms/clrs/proj_p1_sample_1.py
+++ Algorithms/clrs/proj_p1_sample_1.py
@@ -34,9 +34,34 @@ def find_function(func_name,file_name) :
    fp.close()
    return answer
 
+
+
+def parse_log(file_name) :
+
+   try :
+
+      fp = open(file_name)
+
+   except IOError :
+ 
+      raise IOError
+
+   line = fp.readline()
+   functions = []
+   while(line != '') :
+      cre = re.compile(r'VDNetLib')
+      if cre.match(line) :
+         functions.append(line)
+   fp.close()
+   for f in functions:
+      print "line = ", f
+
+
+
 if __name__ == '__main__':
    # expected = 15
-   print sum(5,10)
+   #print sum(5,10)
 
    # expected = 12
-   print find_function('create_dataset','p1.py')
+   #print find_function('create_dataset','p1.py')
+   parse_log('sample.log')
