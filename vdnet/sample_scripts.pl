ca4bfc18989b1709836e7c52b89ba09cfc7be415
Sabesan Saidapet Pachai <ssaidapetpach@htb-1n-eng-dhcp220.eng.vmware.com>
2015-03-05 18:04:20 -0800

    update_March_5_2015

---
+++ vdnet/sample_scripts.pl
@@ -0,0 +1,26 @@
+use strict;
+use warnings;
+
+my $input_str = "/f1/dc1";
+
+## print the input string
+print "input = ",$input_str,"\n";
+
+## split the string on /
+my @split_string = split('/',$input_str);
+
+if($#split_string == 0) {
+
+   print "Invalid datacenter name : need ".
+         "to specify the folder as well\n";
+
+} else {
+
+
+   print "first_string = ",$split_string[0],"\n";
+   print "last_string = ",$split_string[2],"\n";
+
+} 
+
+
+
