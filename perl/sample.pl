507cdc4fc1e9c1b6bb27c7e75f0d5374759b26b8
Sabesan Saidapet Pachai <ssaidapetpach@htb-1n-eng-dhcp278.eng.vmware.com>
2015-03-30 16:13:38 -0700

    update_1_March_30_2015

--- perl/sample.pl
+++ perl/sample.pl
@@ -17,3 +17,15 @@ else {
    print("Not equal\n");
 
 }
+
+my $hash={"me" =>{} };
+my $key="you";
+
+if(ref($hash->{$key}) eq "HASH") {
+
+   print("Hash and key are defined\n");
+
+} else {
+
+   print("Hash and key not defined\n");
+}
