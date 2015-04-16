9437a81a1e3f45edfb89134841544116f9b1b0fd
Sabesan Saidapet Pachai <ssaidapetpach@htb-1n-eng-dhcp215.eng.vmware.com>
2015-04-16 16:37:16 -0700

    update_2_apr_16_2015

--- vdnet/vdnet.py
+++ vdnet/vdnet.py
@@ -1,3 +1,8 @@
-# P1: Connect to a vcenter server and clean up the database
-
-
+if __name__ == "__main__":
+   print "Inside main \n"
+   l = None
+   if   l  : 
+      print "first element = ",l[0],"\n"
+   else :
+      raise Exception("empty list")
+      print "list is empty"
