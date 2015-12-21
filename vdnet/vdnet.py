88096d5b805e8daa6c8f22b610b2e69707925d5c
Sabesan Saidapet Pachai <ssaidapetpach@htb-1n-eng-dhcp165.eng.vmware.com>
2015-12-21 13:25:27 -0800

    Added some commands in vim editor from scratch

--- vdnet/vdnet.py
+++ vdnet/vdnet.py
@@ -1,3 +1,7 @@
+import optparse
+import sys
+
+
 def test_pydict(dict) :
 
    if hasattr(dict,'id') :
@@ -10,5 +14,12 @@ def test_pydict(dict) :
 
 if __name__ == "__main__":
    print "Inside main \n"
-   dict = {'id' : '1','s' : '0'}
-   test_pydict(dict)
+   #dict = {'id' : '1','s' : '0'}
+   #test_pydict(dict)
+   args = sys.argv[1:]
+   usage = "usage: %prog [options]"
+   parser = optparse.OptionParser(usage=usage)
+   parser.add_option("--config",dest="config",action="store",type="string",help="json_config")
+   (cmdOptions,args) = parser.parse_args(args)
+   print "cmdOptions.config : ",cmdOptions.config
+   
