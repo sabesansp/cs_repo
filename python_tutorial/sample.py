8b542c78bc99f7371db4e47cdac3b7ac67c694da
Sabesan Saidapet Pachai <sabesansp@gmail.com>
2015-12-26 09:41:32 -0800

    Initial instructions for python related changes in vim editor

---
+++ python_tutorial/sample.py
@@ -0,0 +1,15 @@
+# This is a sample python file which can be used
+# to teach programming to beginners in python
+
+# Task1 : Adjust vim editor to make sure that each line does not exceed 90 characters,test
+
+def main():
+
+   print "Inside main function: make sure that this line extends 90 character limit\n"
+
+
+if __name__ == '__main__':
+   main()
+
+
+
