9c9e936eb55a3a06527896da6cf2ac16e1653158
Sabesan Saidapet Pachai <ssaidapetpach@htb-1n-eng-dhcp114.eng.vmware.com>
2016-01-14 14:29:35 -0800

    Adding leetcode_solutions folder to repo

---
+++ python_tutorial/leetcode_solutions/TwoSum.py
@@ -0,0 +1,25 @@
+class TwoSum(object):
+
+
+   def twoSum(self, nums, target):
+      """
+      
+      :type nums: list[int]
+      :type target: int
+      :rtype: list[int]
+      """
+
+      if nums is None or len(nums)==0:
+         raise Exception("nums is none/empty")
+      else:
+         # Initialize a dictionary 
+         d = {}
+         
+         
+
+
+
+if __name__ == '__main__':
+   t = TwoSum()
+   t.twoSum([],9)
+           
