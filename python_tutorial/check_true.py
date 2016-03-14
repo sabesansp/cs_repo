ba9b05006a04ee9e52b36ab33d883ab5c8acca34
Sabesan Pachai <sabesanpachai@SPachaiSJC-MBP.local>
2016-03-14 14:14:56 -0700

    Adding check_true.py to the codebase

---
+++ python_tutorial/check_true.py
@@ -0,0 +1,17 @@
+def isTrue(x):
+
+   if x==0:
+      return True
+   else:
+      return False
+
+
+def main():
+
+   if not isTrue(10):
+      print "10 is true"
+   else:
+      print "10 is false"
+
+if __name__ == "__main__":
+   main()
