49a9209b0dd6f8b87e2f53132e11561ffef82b2d
Sabesan Pachai <sabesanpachai@SPachaiSJC-MBP.local>
2016-03-15 17:21:44 -0700

    Added tasks 3 & 4

--- python_tutorial/sample.py
+++ python_tutorial/sample.py
@@ -1,8 +1,12 @@
 # This is a sample python file which can be used
 # to teach programming to beginners in python
 
-# Task1 : Adjust vim editor to make sure that each line does not exceed 90 characters,test
 
+from multiprocessing import Process
+import sys
+
+
+# Task1 : Adjust vim editor to make sure that each line does not exceed 90 characters,test
 def main():
 
    print "Inside main function: make sure that this line extends 90 character limit"
@@ -16,6 +20,28 @@ def is_valid_ipv4_addr(ip):
       return False
    return all(0 <= int(octet) <= 255 for octet in octets)
 
+
+# Task 3: Write a method and spawn this method as a process
+def run_as_process(name):
+   try:
+      vm = None
+      # Inject an error here to check 
+      # what happens to the process when this is run
+      if vm.is_valid():
+         print "vm is valid"
+      print "Inside function run_as_process : %s" %(name)
+   except:
+      print "Unexpected error : ", sys.exc_info()
+      return False
+   return True
+
+
+# Task 4: catch the exception above and return true or false based on the outcome
+def call_process():
+
+   print "Calling run_as_process\n",run_as_process('process:sabesan')
+
+
 if __name__ == '__main__':
    main()
 
@@ -26,3 +52,7 @@ if __name__ == '__main__':
    print is_valid_ipv4_addr('10.456.34.255')
 
 
+   # Run the previous function as a process
+   p = Process(target=call_process, args=()) 
+   p.start()
+   p.join() # this blocks until process terminates 
