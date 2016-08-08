fd622bccd459637cbfc065a0297b4f4c513ae2f9
Sabesan Saidapet Pachai <research@Sabesans-MacBook-Pro.local>
2016-08-07 17:57:03 -0700

    Adding the code for median of two sorted arrays and this solution was accepted

--- python_tutorial/leetcode_solutions/median_two_sorted_arrays/median_two_sorted_arrays.py
+++ python_tutorial/leetcode_solutions/median_two_sorted_arrays/median_two_sorted_arrays.py
@@ -20,30 +20,53 @@ class Solution(object):
         """
         m = len(nums1)
         n = len(nums2)
-        if m==0 and n==0:
-            return None
-        if m>n:
-            return self.findMedianSortedArrays(nums2, nums1)
-        if m==1 and n==1:
-            return float( (nums1[0] + nums2[0])/ 2.0 )
-        if m==2 and n==2:
-            return float( max(nums1[0], nums2[0]) + min(nums1[1], nums2[1]) ) / 2.0
-        if m==1 and n>2:
-            m2 = self.median(nums2)
-            return self.findMedianSortedArrays(nums1, nums2[:n/2+1])
-        if m>2 and n>2:
-            m1 = self.median(nums1)
-            m2 = self.median(nums2)
-            if m1 == m2:
-                return m1
-            elif m1>m2:
-                nums1, nums2 = nums2, nums1
-            return self.findMedianSortedArrays(nums1[m/2 -1: ], nums2[:n/2+1])
-        return None
+        l = m + n
+        if l==0:
+            raise Exception("Invalid input: Empty arrays")
+        k = -1
+        if l%2 == 0:
+            n1 = self.findKthSmallest(nums1, nums2, l/2)
+            n2 = self.findKthSmallest(nums1, nums2, l/2+1)
+            return (n1 + n2)/2.0
+        else:
+            return self.findKthSmallest(nums1, nums2, l/2+1)    
+
+
+    def findKthSmallest(self, nums1, nums2, k):
+        m = len(nums1)
+        n = len(nums2)
+        l = m + n
+        if k<1 or k>l:
+            raise Exception("Invalid value for k. k has to be "  
+                            "between 1 and sum of lengths of arrays")
+        if m==0:
+            return nums2[k-1]
+        elif n==0:
+            return nums1[k-1]
+        elif m>n:
+            # Exchange nums1 and nums2 since m>n
+            return self.findKthSmallest(nums2, nums1, k)
+        elif k==1:
+            return min(nums1[0], nums2[0])
+        else:
+            i = k/2 -1
+            j = -1
+            if k%2 == 0:
+                j = k/2 -1
+            else:
+                j = k/2
+            if i>m-1 or i<0:
+                i = m/2
+                j = k-i-2
+            if nums1[i] <= nums2[j]:
+                return self.findKthSmallest(nums1[i+1:m], nums2[0:j+1], k-i-1)
+            else:
+                return self.findKthSmallest(nums1[0:i+1], nums2[j+1:n], k-j-1)
+                      
 
 if __name__ == '__main__':
 
     s = Solution()
-    nums1 = [1,2,3,4]
-    nums2 = [5,6,7,8]
+    nums1 = [1, 1, 10, 11]
+    nums2 = [1, 2, 4, 5]
     print s.findMedianSortedArrays(nums1, nums2)
